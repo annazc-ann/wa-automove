0105d14738cfe6e61992dd51d7dbe36ba47c116d
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-16 08:15:52 +0000

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -5,8 +5,6 @@ mkdir /sdcard/WA-Media/WA-Audios
 mkdir /sdcard/WA-Media/WA-Documents
 mkdir /sdcard/WA-Media/WA-Images
 mkdir /sdcard/WA-Media/Others
-mkdir .bak
-mv mkdir.sh .bak
 echo 
-echo OK!
+echo OK!👍
 echo
