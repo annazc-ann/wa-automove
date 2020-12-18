4776d32de4c0cb1a60e49134460f4c5bfc67e18a
Ann <75046143+annazc-ann@users.noreply.github.com>
2020-12-18 10:10:36 +0700

    Add files via upload

--- automove.py
+++ automove.py
@@ -10,6 +10,7 @@ Author: Ann
 
 # import the required modules to interact with files and dirs
 import os, shutil 
+from styl import * # styling output
 
 # WhatsApp path directories
 path = "/sdcard/WhatsApp/Media/"
@@ -19,6 +20,12 @@ wdirs = ["WhatsApp Video","WhatsApp Images","WhatsApp Audio","WhatsApp Documents
 dest = "/sdcard/WA-Media/"
 dirs = ["WA-Videos","WA-Images","WA-Audios","WA-Documents","WA-Apks","Others"]
 
+# create destination folder 
+for i in range(len(dirs)):
+   if os.path.exists(dest+dirs[i]) == False:
+      os.makedirs(dest+dirs[i])
+      print(style.OKGREEN+"folder",style.OKBLUE+dest+dirs[i],style.ENDC+style.OKGREEN,"created!")
+
 # declaring arrays of files' formats 
 # you can add your desired file format
 vid_formats = ["mp4","3gp","mkv","avi"]
@@ -27,10 +34,10 @@ aud_formats = ["mp3","aac","m4a"]
 doc_formats = ["pdf","docx","xlsx","bok","txt","doc","pptx"]
 apk_formats = ["apk","xapk"]
 
-formats = [vid_formats,img_formats,aud_formats,doc_formats,apk_formats]
+# formats = [vid_formats,img_formats,aud_formats,doc_formats,apk_formats]
 
 def automove():
-   files = os.listdir() # get list of all files in the current dir
+   files = os.listdir('./') # get list of all files in the current dir
    for f in files:
       if os.path.isfile(f): # strict to moving files only, not folders
 
@@ -40,41 +47,48 @@ def automove():
          # begin to move all files
          if ext in vid_formats:
             shutil.move(f,dest+dirs[0])
-            print("Successfully moved",f,"to",dest+dirs[0])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[0])
          elif ext in img_formats:
             shutil.move(f,dest+dirs[1])
-            print("Successfully moved",f,"to",dest+dirs[1])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[1])
          elif ext in aud_formats:
             shutil.move(f,dest+dirs[2])
-            print("Successfully moved",f,"to",dest+dirs[2])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[2])
          elif ext in doc_formats:
             shutil.move(f,dest+dirs[3])
-            print("Successfully moved",f,"to",dest+dirs[3])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[3])
          elif ext in apk_formats:
             shutil.move(f,dest+dirs[4])
-            print("Successfully moved",f,"to",dest+dirs[4])
-         elif ext != formats:
-            shutil.move(f,dest+dirs[5])
-            print("Successfully moved",f,"to",dest+dirs[5])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[4])
          else:
-            print("Media not found")
+            shutil.move(f,dest+dirs[5])
+            print(style.OKGREEN+"Successfully moved",style.HEADER+f,style.OKGREEN+"to", style.OKBLUE+dest+dirs[5])
+
+# logical statement
+while True:
+   print(style.WARNING+"\nDo you want to start moving files? [Yes/No] ")
+   ans = input(f">{style.OKGREEN} ")
+   ans = ans.lower()
+   if ans == "y" or ans == "yes" or ans == "yes " or ans == "y ":
+      # changing between all dirs inside WhatsApp path
+      for i in range(len(wdirs)):
+         os.chdir(path+wdirs[i])
+         automove()
+         
+      print(style.OKGREEN+"\n+-+-+-+-+-+\nIt's DONE √\n+-+-+-+-+-+\n"+style.ENDC) 
+      print(style.WARNING+"============================================================\n😊 you can find your files in 👉",style.BOLD+style.OKBLUE+"internal storage > WA-Media"+style.ENDC,style.WARNING+"\n============================================================"+style.ENDC)
+      break
+   elif ans == "n" or ans == "no" or ans == "no " or ans == "n ":
+      print("\n____________________\n< see you later 👏 >\n--------------------\n        \   ^__^\n         \  (oo)\_______\n            (__)\       )\/\\\n                ||----w |\n                ||     ||w\n")
+      break
+   elif ans == "":
+      print(style.FAIL+"[ERROR] no input! (yes/no/y/n) ???\n")
+   elif ans != "y" or ans != "yes" or ans != "y " or ans != "yes " or ans != "n" or ans != "no" or ans != "n " or ans != "no ":
+      print(style.FAIL+"\n[ERROR] input invalid. (yes/no/y/n)?!")
+   
 
 # make it possible to run on background
-#while True:
 
-   # changing between all dirs inside WhatsApp path
-os.chdir(path+wdirs[0])
-automove()
-os.chdir(path+wdirs[1])
-automove()
-os.chdir(path+wdirs[2])
-automove()
-os.chdir(path+wdirs[3])
-automove()
-os.chdir(path+wdirs[4])
-automove()
-
-print("\nIt's done ☑\n") 
 
 
 
